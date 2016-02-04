f5e806dd14bdba2fe2dd34225ce1cf9f759e6267
Hayden Parker <haydenkparker@gmail.com>
2016-02-04 01:14:49 -0800

    date test

--- lib/common/date.go
+++ lib/common/date.go
@@ -6,7 +6,7 @@ import (
 
 type Date [8]byte
 
-func GoDate(date Date) time.Time {
+func (date Date) Time() time.Time {
 	seconds := Integer(date[:])
 	return time.Unix(0, int64(seconds*1000000))
 }
