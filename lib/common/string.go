84073c20e3e15723406664b023145df6c7e1288e
Hayden Parker <haydenkparker@gmail.com>
2016-02-05 22:35:37 -0800

    using go string type for utf8 data

--- lib/common/string.go
+++ lib/common/string.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 )
 
-type String []rune
+type String []byte
 
 func (str String) Length() (int, error) {
 	if len(str) == 0 {
@@ -24,48 +24,43 @@ func (str String) Length() (int, error) {
 	return length, nil
 }
 
-func (str String) Data() ([]rune, error) {
+func (str String) Data() (string, error) {
 	length, err := str.Length()
 	if err != nil {
 		switch err.Error() {
 		case "error parsing string: zero length":
-			return make([]rune, 0), err
+			return "", err
 		case "string parsing warning: string data is shorter than specified by length":
-			return str[1:], err
+			return string(str[1:]), err
 		case "string parsing warning: string contains data beyond length":
-			return str[1 : length+1], err
+			return string(str[1 : length+1]), err
 		}
 	}
-	return str[1:], nil
+	return string(str[1:]), nil
 }
 
-func (str String) GoString() (string, error) {
-	content, err := str.Data()
-	return string(content), err
-}
-
-func ToI2PString(data []rune) (String, error) {
+func ToI2PString(data string) (String, error) {
 	data_len := len(data)
 	if data_len >= 256 {
-		return make([]rune, 0), errors.New("cannot store that much data in I2P string")
+		return make([]byte, 0), errors.New("cannot store that much data in I2P string")
 	}
-	i2p_string := []rune{rune(data_len)}
-	i2p_string = append(i2p_string, data...)
+	i2p_string := []byte{byte(data_len)}
+	i2p_string = append(i2p_string, []byte(data)...)
 	return String(i2p_string), nil
 }
 
-func ReadString(data []rune) (String, []rune, error) {
+func ReadString(data []byte) (String, []byte, error) {
 	str := String(data)
 	length, err := String(data).Length()
 	if err != nil {
 		switch err.Error() {
 		case "error parsing string: zero length":
-			return String{}, make([]rune, 0), err
+			return String{}, make([]byte, 0), err
 		case "string parsing warning: string data is shorter than specified by length":
-			return str, make([]rune, 0), err
+			return str, make([]byte, 0), err
 		case "string parsing warning: string contains data beyond length":
 			return String(str[:length+1]), str[length+1:], err
 		}
 	}
-	return str, make([]rune, 0), nil
+	return str, make([]byte, 0), nil
 }
